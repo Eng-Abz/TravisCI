745f2ebb7f11bd5fde1abc5c3463011d1e54c656
Abdulaziz Mohameden <eng.abz1@gmail.com>
2022-11-02 07:35:29 +0300

    add test class

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test{
+    public static void main(String[] args) {
+        // for (int i = 0; i<200 ;i++) {
+        //     System.out.println(i);
+        // }
+        // System.out.println("--");
+    
+        System.out.println("add" +add());
+        
+    }
+    
+    
+    public static int add() {
+    
+        int a = 3;
+        int b = 2;
+    
+        return a+b;
+    
+        
+    }
+    
+    } 
